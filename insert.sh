99bcc23115bb5181a33073f1a351579deb315ac1
Nicholas Parise <1treasurehound@gmail.com>
2025-09-28 16:16:14 -0400

    fixed import script

--- insert.sh
+++ insert.sh
@@ -1,2 +1,4 @@
-time pigz -dc links.csv.gz | sqlite3 wikipedia.sqlite ".read ./sql/links.sql"
-time pigz -dc pages.tsv.gz | sqlite3 wikipedia.sqlite ".read ./sql/pages.sql"
+time gzip -dc links.csv.gz
+time gzip -dc pages.tsv.gz
+sqlite3 wikipedia.sqlite ".read ./sql/links.sql"
+sqlite3 wikipedia.sqlite ".read ./sql/pages.sql"
